c0a4e7d5bc7e84b7acc988a8ede293d8bf8d26e0
jsalicetti <jessicasalicetti@yahoo.com>
2024-09-03 10:22:25 -0400

    completed

---
+++ spring-boot-actuator-example/src/test/java/com/actutatorDemo/spring_boot_actuator_example/SpringBootActuatorExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actutatorDemo.spring_boot_actuator_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
